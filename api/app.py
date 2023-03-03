af1c3c1756cc2209892fff2f4b2fe8e99706d3fa
Gustavo Cabral <gmcabral@gmail.com>
2023-03-03 20:58:13 -0300

    move port 80

--- api/app.py
+++ api/app.py
@@ -10,4 +10,4 @@ app.register_blueprint(instrumentos_blueprint)
 # app.register_blueprint(items_blueprint)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=80)
